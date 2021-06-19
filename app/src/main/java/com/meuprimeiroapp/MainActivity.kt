696789fecacc65ddcfaed183743c6820d6a3c37a
samueladaosantos <83379137+samueladaosantos@users.noreply.github.com>
2021-06-18 21:34:32 -0300

    teste

--- app/src/main/java/com/meuprimeiroapp/MainActivity.kt
+++ app/src/main/java/com/meuprimeiroapp/MainActivity.kt
@@ -2,10 +2,41 @@ package com.meuprimeiroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("lifecycle", "onCreat")
+
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i("lifecycle", "onResume")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i("lifecycle", "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i("lifecycle", "onPouse")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i("lifecycle", "onStop")
     }
 }
---
+++ app/src/main/java/com/meuprimeiroapp/Tela01Activity.kt
@@ -0,0 +1,11 @@
+package com.meuprimeiroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela01Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela01)
+    }
+}
